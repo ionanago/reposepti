3d73ff3c83cb895339cebba8c5ae28d895a02a8a
ionanago <51776373+ionanago@users.noreply.github.com>
2019-09-04 13:34:48 +0200

    jdbc

--- ejemplojdbc/src/ejemplojdbc/ejemplojdbc.java
+++ ejemplojdbc/src/ejemplojdbc/ejemplojdbc.java
@@ -3,7 +3,12 @@ import java.sql.*;
 
 public class ejemplojdbc {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		//new com.mysql.cj.jdbc.Driver();
+		
+		String uri = "jdbc://localhost:3307/libreria?&serverTimezone=UTC";
+		Connection con = DriverManager.getConnection(uri,"root","admin");
+		System.out.println("Connectado");
 		
 		
 
